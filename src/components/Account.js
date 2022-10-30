8dabfe94e31ed390c4fa4e79b9ff427f0588f20d
Austin M. Reppert <austinmreppert@protonmail.com>
2022-10-30 12:56:16 -0400

    Improve error handling when failing to auth with a token.

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { Card, ListGroup } from 'react-bootstrap';
+import { ListGroup } from 'react-bootstrap';
 
 import { ME } from '../queries';
 
@@ -8,16 +8,15 @@ const Account = () => {
   const me = useQuery(ME);
 
   if (me.loading) return <div>Loading...</div>;
-
   return (
     <div className='bg-white border border-dark border-opacity-25 p-5 rounded-4'>
       <h2>Account</h2>
         <ListGroup variant="flush">
-        <ListGroup.Item>Username: {me.data ? me.data.me.username : ''}</ListGroup.Item>
-        <ListGroup.Item>Name: {me.data ? me.data.me.name : ''}</ListGroup.Item>
-        <ListGroup.Item>Role: {me.data ? me.data.me.role : ''}</ListGroup.Item>
-        <ListGroup.Item>Attendance Count: {me.data ? me.data.me.attendanceCount : ''}</ListGroup.Item>
-        <ListGroup.Item>id: {me.data ? me.data.me.id : ''}</ListGroup.Item>
+        <ListGroup.Item>Username: {me.data.me ? me.data.me.username : ''}</ListGroup.Item>
+        <ListGroup.Item>Name: {me.data.me ? me.data.me.name : ''}</ListGroup.Item>
+        <ListGroup.Item>Role: {me.data.me ? me.data.me.role : ''}</ListGroup.Item>
+        <ListGroup.Item>Attendance Count: {me.data.me ? me.data.me.attendanceCount : ''}</ListGroup.Item>
+        <ListGroup.Item>id: {me.data.me ? me.data.me.id : ''}</ListGroup.Item>
       </ListGroup>
     </div>
   );
