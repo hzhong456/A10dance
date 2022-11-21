167cb4424c64e28997e607145af7271c74a472ba
hzhong456 <68069530+hzhong456@users.noreply.github.com>
2022-11-21 17:04:44 -0500

    Fixed Check-In Button Functionality

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,58 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+// import React from 'react';
+// import { Button } from 'react-bootstrap';
 
-const Dashboard = () => (
-  <div className='bg-white border border-dark border-opacity-25 p-5 rounded-4'>
-    <h2>Student Dashboard</h2>
-    <div className='col text-center my-5'>
-      <Button type='button' variant='success'>Check In</Button>
+import { useQuery } from '@apollo/client';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { Form, Button } from 'react-bootstrap';
+import { ATTENDED } from '../queries';
+import Notification from './Notification';
+import { ME } from '../queries';
+
+const Dashboard = () => {
+
+  const me = useQuery(ME);
+  const [message, setMessage] = useState(null);
+  let messageTimeout;
+
+  const [attended] = useMutation(ATTENDED, {
+    onCompleted: () => {
+      setMessage(['added', 'Attendance Added']);
+      clearTimeout(messageTimeout);
+      messageTimeout = setTimeout(() => {
+        setMessage(null);
+      }, 5000);
+    },
+    onError: (err) => {
+      setMessage(['error', `${err}`]);
+      clearTimeout(messageTimeout);
+      messageTimeout = setTimeout(() => {
+        setMessage(null);
+      }, 5000);
+    },
+  });
+
+  const submit = async (event) => {
+    event.preventDefault();
+
+    attended({
+      variables: { username: me.data.me.username },
+    });
+  };
+
+  return (
+    <div className='bg-white border border-dark border-opacity-25 p-5 rounded-4'>
+      <h2>Student Dashboard</h2>
+      <Notification message={message} />
+      <Form onSubmit={submit}>
+        <div className='col text-center my-5'>
+          <Button type='submit' variant='success'>Check In</Button>
+        </div>
+      </Form>
     </div>
-  </div>
-);
+
+  );
+
+};
 
 export default Dashboard;
