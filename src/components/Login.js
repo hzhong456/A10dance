8acc6f0a9fb0f35478a1f8f12defa67b721a51c0
hzhong456 <68069530+hzhong456@users.noreply.github.com>
2022-11-21 17:05:24 -0500

    Added Roles Property

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,9 @@
-import React, {useState, useEffect} from 'react';
-import {useMutation} from '@apollo/client';
-import {useNavigate} from 'react-router-dom';
-import {Form, Button} from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { useMutation } from '@apollo/client';
+import { useNavigate } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
 
-import {LOGIN} from '../queries';
+import { LOGIN } from '../queries';
 import Notification from './Notification';
 
 const Login = (props) => {
@@ -29,12 +29,21 @@ const Login = (props) => {
   useEffect(() => {
     if (result.data) {
       const token = result.data.login.value;
-      const {name} = result.data.login.user;
+      const { name } = result.data.login.user;
+      const role = result.data.login.user.role;
       props.setToken(token);
       props.setName(name);
+      props.setRole(role);
       localStorage.setItem('token', token);
       localStorage.setItem('name', name);
-      navigate('/dashboard');
+      localStorage.setItem('role', role)
+
+      if (role === "Student") {
+        navigate('/dashboard')
+      }
+      else {
+        navigate('/attendance-list')
+      }
     }
   }, [props, navigate, result.data]);
 
@@ -42,22 +51,22 @@ const Login = (props) => {
     event.preventDefault();
 
     login({
-      variables: {username, password},
+      variables: { username, password },
     });
   };
 
   return (
     <div className='bg-white border border-dark border-opacity-25 p-5 rounded-4'>
       <h2>Login</h2>
-      <Notification message={message}/>
+      <Notification message={message} />
       <Form onSubmit={submit} className='login-form'>
         <Form.Group className='mb-3'>
           <Form.Label>Username</Form.Label>
-          <Form.Control value={username} onChange={({target}) => setUsername(target.value)}/>
+          <Form.Control value={username} onChange={({ target }) => setUsername(target.value)} />
         </Form.Group>
         <Form.Group className='mb-3'>
           <Form.Label>Password</Form.Label>
-          <Form.Control type='password' value={password} onChange={({target}) => setPassword(target.value)}/>
+          <Form.Control type='password' value={password} onChange={({ target }) => setPassword(target.value)} />
         </Form.Group>
         <Button type='submit' variant='primary'>Login</Button>
       </Form>
