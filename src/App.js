a2aebd1df120aa74a59d7ff091e397a592bdc061
hzhong456 <68069530+hzhong456@users.noreply.github.com>
2022-11-21 17:07:22 -0500

    Added role properties and role routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import {Container} from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 import { useApolloClient } from '@apollo/client';
 
@@ -14,30 +14,34 @@ import Register from './components/Register';
 
 const App = () => {
   const [name, setName] = useState('');
+  const [role, setRole] = useState('');
   const [token, setToken] = useState(null);
   const client = useApolloClient();
 
   useEffect(() => {
     const storageToken = localStorage.getItem('token');
     const nameToken = localStorage.getItem('name');
-    if (storageToken && nameToken) {
+    const roleToken = localStorage.getItem('role');
+    if (storageToken && nameToken && roleToken) {
       setToken(storageToken);
       setName(nameToken);
+      setRole(roleToken);
     }
   }, []);
 
   return (
     <div>
-      <Menu token={token} setToken={setToken} client={client} name={name} setName={setName}/>
+      <Menu token={token} setToken={setToken} client={client} name={name} setName={setName} role={role} setRole={setRole}/>
       <Container className='mt-5 d-flex justify-content-center'>
         <Routes>
           <Route path="/" element={<Welcome />} />
-          {!token && <Route path="/login" element={<Login setToken={setToken} setName={setName} />} />}
+          {!token && <Route path="/login" element={<Login setToken={setToken} setName={setName} setRole={setRole} />} />}
           {!token && <Route path="/register" element={<Register />} />}
 
-          {token ? <Route path="/dashboard" element={<Dashboard />} /> : null}
-          {token ? <Route path="/attendance-list" element={<AttendanceList />} /> : null}
+          {(token && localStorage.getItem('role') === "Student") ? <Route path="/dashboard" element={<Dashboard />} /> : null}
+          {(token && localStorage.getItem('role') === "Professor") ? <Route path="/attendance-list" element={<AttendanceList />} /> : null}
           {token ? <Route path="/account" element={<Account />} /> : null}
+
         </Routes>
       </Container>
     </div>
