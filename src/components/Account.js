3de1ddc113f28e2f69b6218bb4117e1d2abe88db
hzhong456 <68069530+hzhong456@users.noreply.github.com>
2022-11-21 17:06:16 -0500

    Updated  formatting of text

--- src/components/Account.js
+++ src/components/Account.js
@@ -11,12 +11,12 @@ const Account = () => {
   return (
     <div className='bg-white border border-dark border-opacity-25 p-5 rounded-4'>
       <h2>Account</h2>
-        <ListGroup variant="flush">
-        <ListGroup.Item>Username: {me.data && me.data.me ? me.data.me.username : ''}</ListGroup.Item>
-        <ListGroup.Item>Name: {me.data && me.data.me ? me.data.me.name : ''}</ListGroup.Item>
-        <ListGroup.Item>Role: {me.data && me.data.me ? me.data.me.role : ''}</ListGroup.Item>
-        <ListGroup.Item>Attendance Count: {me.data && me.data.me ? me.data.me.attendanceCount : ''}</ListGroup.Item>
-        <ListGroup.Item>id: {me.data && me.data.me ? me.data.me.id : ''}</ListGroup.Item>
+      <ListGroup variant="flush">
+        <ListGroup.Item><b>Username:</b> {me.data && me.data.me ? me.data.me.username : ''}</ListGroup.Item>
+        <ListGroup.Item><b>Name: </b> {me.data && me.data.me ? me.data.me.name : ''}</ListGroup.Item>
+        <ListGroup.Item><b>Role:</b> {me.data && me.data.me ? me.data.me.role : ''}</ListGroup.Item>
+        <ListGroup.Item><b>Attendance Count:</b> {me.data && me.data.me ? me.data.me.attendanceCount : ''}</ListGroup.Item>
+        <ListGroup.Item><b>ID:</b> {me.data && me.data.me ? me.data.me.id : ''}</ListGroup.Item>
       </ListGroup>
     </div>
   );
